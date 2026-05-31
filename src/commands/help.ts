99070bf7cdebc3c3d6c2a25a062c10996a9aba8c
Sdriver1 <sjdriver08@gmail.com>
2026-05-31 04:22:01 +0000

    Updating help with new commands and format

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,28 +1,45 @@
 import type { Command } from "./types";
 import { commands } from "./index";
 
+const HELP_TEXT = [
+  "Pridebot Commands",
+  "",
+  "Fun",
+  "  !gaydar  !bidar  !lesdar  !transdar  !queerdar",
+  "",
+  "Roles",
+  "  !rolesetup  !rolegive  !roleremove  !roleremoveall",
+  "  !roleglobal  !rolelist  !roledelete",
+  "",
+  "Support",
+  "  !comingout  !mentalhealth  !transresources",
+  "",
+  "Tools",
+  "  !help  !invite  !ping",
+  "",
+  "Use !help <command> for details on a specific command.",
+  "Support server: https://osm.pm/i/JFfIIoNjbXK5x2D9",
+  "GitHub: https://github.com/Pridebot-Systems/Pridebot-OSM",
+].join("\n");
+
 export const help: Command = {
   name: "help",
-  description:
-    "Shows a list of commands, or info about a specific command.",
+  description: "Shows a list of commands, or info about a specific command.",
   usage: "!help [command]",
   execute(bot, message, args) {
-    if (args.length > 0) {
-      const cmd = commands.get(args[0].toLowerCase());
+    const query = args[0];
+    if (query) {
+      const cmd = commands.get(query.toLowerCase());
       if (cmd) {
         bot.reply(
           message,
           `${cmd.name} - ${cmd.description}\nUsage: ${cmd.usage}`,
         );
       } else {
-        bot.reply(message, `Unknown command: ${args[0]}`);
+        bot.reply(message, `Unknown command: ${query}`);
       }
       return;
     }
-    const commandList = Array.from(commands.keys()).join(", ");
-    bot.reply(
-      message,
-      `Available commands: ${commandList}\nUse !help <command> for more info. \n\nSupport server: https://osm.pm/i/JFfIIoNjbXK5x2D9\nGitHub repo: https://github.com/Pridebot-Systems/Pridebot-OSM`,
-    );
+    bot.reply(message, HELP_TEXT);
   },
 };
